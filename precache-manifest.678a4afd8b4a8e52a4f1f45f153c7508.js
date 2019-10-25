097bed5b0e16ee58a6337bed29552b68a9df34ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-25 07:27:48 +0000

    Deploy abdullasulaiman/travis-reactjs-example to github.com/abdullasulaiman/travis-reactjs-example.git:gh-pages

---
+++ precache-manifest.678a4afd8b4a8e52a4f1f45f153c7508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ba77dacad9a0b47d40817da441d652",
+    "url": "/travis-reactjs-example/index.html"
+  },
+  {
+    "revision": "e121473af31113b8edb3",
+    "url": "/travis-reactjs-example/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "60d2647c042a51d2336e",
+    "url": "/travis-reactjs-example/static/js/2.c2929629.chunk.js"
+  },
+  {
+    "revision": "e121473af31113b8edb3",
+    "url": "/travis-reactjs-example/static/js/main.66a2808d.chunk.js"
+  },
+  {
+    "revision": "447f929d0f7bba284a79",
+    "url": "/travis-reactjs-example/static/js/runtime-main.92381f40.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/travis-reactjs-example/static/media/logo.25bf045c.svg"
+  }
+]);
